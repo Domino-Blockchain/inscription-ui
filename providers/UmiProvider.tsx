6eb7a38b57a23189545b51081846feeb4fc1f193
Artyom Titov <at@redsteep.com>
2024-06-27 20:16:35 +0300

    fix: override program IDs

--- providers/UmiProvider.tsx
+++ providers/UmiProvider.tsx
@@ -6,6 +6,11 @@ import { ReactNode, useMemo } from 'react';
 import { nftStorageUploader } from '@metaplex-foundation/umi-uploader-nft-storage';
 import { mplInscription } from '@metaplex-foundation/mpl-inscription';
 import { UmiContext } from './useUmi';
+import { Umi, publicKey } from '@metaplex-foundation/umi';
+import {
+  createSplTokenProgram,
+  createSplAssociatedTokenProgram,
+} from '@metaplex-foundation/mpl-toolbox';
 
 export const UmiProvider = ({ children }: { children: ReactNode }) => {
   const wallet = useWallet();
@@ -15,15 +20,25 @@ export const UmiProvider = ({ children }: { children: ReactNode }) => {
   //   console.error("Add your nft.storage Token to .env!");
   //   nftStorageToken = 'AddYourTokenHere';
   // }
-  const umi = useMemo(
-    () =>
-      createUmi(connection)
-        .use(walletAdapterIdentity(wallet))
-        .use(mplTokenMetadata())
-        .use(nftStorageUploader({ token: process.env.NEXT_PUBLIC_NFTSTORAGE_TOKEN! }))
-        .use(mplInscription()),
-    [wallet, connection]
-  );
+  const umi: Umi = useMemo(() => {
+    const _umi = createUmi(connection)
+      .use(walletAdapterIdentity(wallet))
+      .use(mplTokenMetadata())
+      .use(nftStorageUploader({ token: process.env.NEXT_PUBLIC_NFTSTORAGE_TOKEN! }))
+      .use(mplInscription());
+
+    _umi.programs.add({
+      ...createSplTokenProgram(),
+      publicKey: publicKey('TokenAAGbeQq5tGW2r5RoR3oauzN2EkNFiHNPw9q34s'),
+    });
+
+    _umi.programs.add({
+      ...createSplAssociatedTokenProgram(),
+      publicKey: publicKey('Dt8fRCpjeV6JDemhPmtcTKijgKdPxXHn9Wo9cXY5agtG'),
+    });
+
+    return umi;
+  }, [wallet, connection]);
 
   return <UmiContext.Provider value={{ umi }}>{children}</UmiContext.Provider>;
 };
