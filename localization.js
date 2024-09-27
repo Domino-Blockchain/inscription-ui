01f183b05d95af3d7812a54941b78e3c055e5a73
Vladimir Shishov <dragvs@gmail.com>
2024-09-27 15:43:40 +0200

    Add translations for Chinese Traditional

--- localization.js
+++ localization.js
@@ -3,11 +3,11 @@
 import LocalizedStrings from 'react-localization';
 
 import english from './dictionaries/en';
-// import zhHant from './dictionaries/zh_Hant';
+import zhHant from './dictionaries/zh_Hant';
 
 const strings = new LocalizedStrings({
   en: english,
-  // zh_Hant: zhHant,
+  zh_Hant: zhHant,
 });
 
 export default strings;
